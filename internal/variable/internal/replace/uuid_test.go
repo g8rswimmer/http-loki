08cbd4fb984d7d0f1054ba94aff0dc4a2724aff4
Sean Justice <seanjustice@Seans-MacBook-Air.local>
2023-10-01 15:03:53 -0700

    added the replace uuid test

---
+++ internal/variable/internal/replace/uuid_test.go
@@ -0,0 +1,62 @@
+package replace
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func TestUUID(t *testing.T) {
+	type args struct {
+		in0  string
+		resp any
+		path string
+		in3  []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				resp: map[string]any{
+					"id": 42,
+				},
+				path: "id",
+			},
+			want:    "id",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp, err := json.Marshal(tt.args.resp)
+			if err != nil {
+				t.Errorf("response encoding error %v", err)
+				return
+			}
+			newResp, err := UUID(tt.args.in0, string(resp), tt.args.path, tt.args.in3)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UUID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			var got map[string]any
+			if err := json.Unmarshal([]byte(newResp), &got); err != nil {
+				t.Errorf("new response encoding %v", err)
+				return
+			}
+			v, ok := got[tt.want].(string)
+			if !ok {
+				t.Errorf("new response uuid assert %T", got[tt.want])
+				return
+			}
+			if err := validator.New().Var(v, "uuid4"); err != nil {
+				t.Errorf("new response uuid validation error %s", v)
+			}
+		})
+	}
+}
